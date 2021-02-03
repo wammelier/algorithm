aa0eb770d869d3a113d18861a145c7845b1f1ab4
wammelier <wammelier@DESKTOP-7CN25AT.localdomain>
2021-02-03 16:54:25 +0900

    PFS 알고리즘 풀이

---
+++ src/algorithm/Algorithm.java
@@ -0,0 +1,31 @@
+package algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Algorithm {
+
+	public static Integer binarySearch(final List<Integer> source, final Integer target, final Integer min, final Integer max) {
+
+		// 상태값 설정
+		Integer index = (min + max) / 2;
+		// 종료 조건
+		if (target.equals(source.get(index)))
+			return index;
+
+		return target < source.get(index) ? binarySearch(source, target, min, index - 1) : // 왼쪽
+				binarySearch(source, target, index + 1, max); // 오른쪽
+	}
+
+	public static void main(final String[] args) {
+		List<Integer> nums = new ArrayList<>();
+		nums.add(1);
+		nums.add(2);
+		nums.add(3);
+		nums.add(4);
+		nums.add(5);
+
+		System.out.println(binarySearch(nums, 5, 0, nums.size() - 1));
+	}
+
+}
