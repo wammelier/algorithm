0d5bafd3c5599eb4fe40109f9235d93588490173
wammelier <royhan4417@gmail.com>
2021-02-04 23:21:05 +0900

    별찍기 for문을 줄여보자..

--- src/algorithm/StarShootinHourglass.java
+++ src/algorithm/StarShootinHourglass.java
@@ -1,6 +1,6 @@
 package algorithm;
 
-public class StarShootingDia {
+public class StarShootinHourglass {
 
 	public static void main(final String[] args) {
 		// TODO Auto-generated method stub
@@ -8,10 +8,10 @@ public class StarShootingDia {
 		int n = 0;
 		for (int i = 0; i < 7; i++) {
 			if(i < 4)
-				for (int k = 0; k < i-3; k++)
+				for (int k = i; k < 3; k++)
 					System.out.print(" ");
 			else
-				for (int k = i; k < 3; k++)
+				for (int k = 0; k < i-3; k++)
 					System.out.print(" ");
 			if (i < 4)
 				for (int j = 0; j <= i * 2; j++)
