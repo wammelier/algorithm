06d487d7ccc312662816c63041f32e8223ca0d70
wammelier <royhan4417@gmail.com>
2021-02-11 20:53:55 +0900

    구구단 가로 찍기 개행 까지 포함

--- src/algorithm/MultiplicationTable.java
+++ src/algorithm/MultiplicationTable.java
@@ -19,21 +19,29 @@ public class MultiplicationTable {
 
 	public void multiple(final Integer number) {
 
-		int	num1	= number;
+//		int	num1	= number;
 		int	value	= 1;
 
 		for (int i = 1; i <= 3; i++) {
 			if (i != 1 && (i - 1) % 3 == 0) {
-				num1	= num1 + 2;
+//				num1	= num1 + 2;
 				value	= 1;
 			}
+			
+			for(int j = 2; j <= 9; j++) {
+				for (int k = value; k <= value + 2; k++) {
+					System.out.printf("%3d", j * k);
+				}
+				System.out.print("   ");
+				
+			}
 
-			for (int j = value; j <= value + 2; j++)
-				System.out.printf("%3d", num1 * j);
-			System.out.print("    ");
-
-			for (int k = value; k <= value + 2; k++)
-				System.out.printf("%3d", (num1 + 1) * k);
+//			for (int j = value; j <= value + 2; j++)
+//				System.out.printf("%3d", num1 * j);
+//			System.out.print("    ");
+//
+//			for (int k = value; k <= value + 2; k++)
+//				System.out.printf("%3d", (num1 + 1) * k);
  
 			value = value + 3;
 			System.out.println("");
