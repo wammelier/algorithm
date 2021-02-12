583158d43b7062e634390f1d53d0f122364a39b0
wammelier <royhan4417@gmail.com>
2021-02-12 22:11:32 +0900

    알고리즘 공부

--- src/algorithm/MultiplicationTable.java
+++ src/algorithm/MultiplicationTable.java
@@ -19,21 +19,26 @@ public class MultiplicationTable {
 
 	public void multiple(final Integer number) {
 
-//		int	num1	= number;
+		int start = 2;
+		int	num1	= number;
 		int	value	= 1;
+		int cal = 9/number;
+		
+		if(number == 9) {
+			
+		}
 
-		for (int i = 1; i <= 3; i++) {
-			if (i != 1 && (i - 1) % 3 == 0) {
+		for (int i = 1; i <= 3*cal; i++) {
+			if (i != 1 && (i - 1) % 3   == 0) {
 //				num1	= num1 + 2;
 				value	= 1;
 			}
 			
-			for(int j = 2; j <= 9; j++) {
+			for(int j = start; j <= num1+1; j++) {
 				for (int k = value; k <= value + 2; k++) {
 					System.out.printf("%3d", j * k);
 				}
 				System.out.print("   ");
-				
 			}
 
 //			for (int j = value; j <= value + 2; j++)
